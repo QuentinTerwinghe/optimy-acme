982ad65513cf3ce32809c55a3cdc735683e1d4ae
Quentin <q.terwinghe@gmail.com>
2025-11-10 17:42:04 +0100

    test: fix cache being not reset

--- www/tests/TestCase.php
+++ www/tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Setup the test environment.
+     *
+     * This method is called before each test method is executed.
+     * It resets the permission cache to prevent cache pollution between tests.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Reset cached roles and permissions before each test
+        // This prevents cache pollution between tests when using Spatie Laravel-Permission
+        // Even with CACHE_STORE=array, the PermissionRegistrar maintains an in-memory cache
+        $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
 }
